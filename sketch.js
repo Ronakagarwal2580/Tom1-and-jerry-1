e4bbd5611d76a40f1ea6d807a14c2f4c45e57188
Ronakagarwal2580 <78287089+Ronakagarwal2580@users.noreply.github.com>
2021-03-15 21:03:06 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,97 +1,41 @@
-var backgroundImg,background1;
-var catImg,cat1,mouseImg,mouse1;
-var mouse2,mouse3,mouseSprite;
-var catimg;
-var cat2,cat3;
+var boy, ball;
 
-function preload() {
+function setup() {
+  createCanvas(800, 400);
+  boy = createSprite(400, 200, 50, 50);
+  boy.shapeColor = "red";
 
-    //load the images here
-    backgroundImg = loadImage("images/garden.png")
-    catImg = loadImage("images/cat2.png")
-    catimg = loadImage("images/cat1.png");
-    cat2 = loadImage("images/cat3.png");
-    cat3 = loadImage("images/cat4.png")
 
-    mouseImg = loadImage("images/mouse1.png");
-    mouse1 = loadImage("images/mouse2.png");
-    mouse2 = loadImage("images/mouse3.png");
-    mouse3 = loadImage("images/mouse4.png");
-   
-   
-}
-
-function setup(){
-
-    createCanvas(950,700);
-    //create tom and jerry sprites here
-
-    background1 = createSprite(480,350,20,30);
-    background1.addImage(backgroundImg);
-
-    cat1 = createSprite(850,350,10,10);
-    cat1.addImage(catimg);
-    cat1.scale = 0.10;
-
-    mouseSprite = createSprite(150,580,5,5);
-    mouseSprite.addImage(mouseImg);
-    mouseSprite.scale = 0.10;
-
-    mouseSprite.setCollider("circle",0,0,50);
-    mouseSprite.debug = true;
+  ball = createSprite(200, 200, 50, 50);
+  ball.shapeColor = "green";
+  ball.velocityX = 5;
+  // ball.velocityY = 5;
 }
 
 function draw() {
+  background("black");
 
-    background("black");
-    //Write condition here to evalute if tom and jerry collide
-
-    
-
-    if(keyDown("left")){
-
-        //cat1.addImage(catImg);
-        cat1.addImage(catImg,cat2);
-        cat1.velocityX = cat1.velocityX - 0.5;
-        cat1.velocityY = 0;
-        mouseSprite.addImage(mouse2);
-    }
-    if(keyDown("up")){
-
-        cat1.addImage(cat2);
-        cat1.velocityY = cat1.velocityY - 0.5;
-        cat1.velocityX = 0;
-        mouseSprite.addImage(mouse1);  
-    }
-    if(keyDown("down")){
-
-        cat1.addImage(cat2);
-        cat1.velocityY = cat1.velocityY + 0.5;
-        cat1.velocityX = 0;
-        mouseSprite.addImage(mouse1);   
-
-    }
-    if(keyDown("right")){
-
-        cat1.addImage(catImg,cat2);
-        cat1.velocityX = cat1.velocityX + 0.5;
-        cat1.velocityY = 0;
-        mouseSprite.addImage(mouse2);
-    }
-    if(cat1.isTouching(mouseSprite)){
-
-        cat1.setVelocity(0,0);
-        cat1.addImage(cat3);
-        mouseSprite.addImage(mouse3);
-        cat1.x = 160;
-        
-    }
+  boy.x = mouseX;
+  boy.y = mouseY;
 
 
+  ronak();
 
+  drawSprites();
 
-    drawSprites();
 }
+function ronak() {
+  if (boy.x - ball.x <= ball.width / 2 + boy.width / 2 &&
+    ball.x - boy.x <= ball.width / 2 + boy.width / 2 &&
+    boy.y - ball.y <= ball.height / 2 + boy.height / 2 &&
+    ball.y - boy.y <= ball.height / 2 + boy.height / 2) {
+    ball.shapeColor = "red";
+    ball.velocityX = -ball.velocityX;
 
+  } else {
+    ball.shapeColor = "green";
+    ball.velocityX = ball.velocityX;
+  }
+}
 
 
